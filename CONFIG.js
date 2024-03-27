aab1eb4f407a1db8c596237fcc9182f8d1b489a9
xinchaofbanj <147034000+xinchaofbanj@users.noreply.github.com>
2024-03-27 16:03:33 +0700

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Em chào cô, em có điều muốn nói. </br> ******** !",
+  descriptionHeader:
+    "Cô ấn đi ạ. Đằng sau vẫn còn ****.",
+  buttonYes: " Đồng ý ",
+  buttonNo: "Không Đồng ý ",
+  titleModar: " Em Cảm ơn.",
+  descriptionModar:
+    "Mong cô tha thứ cho em. Em không thể tham gia thi học sinh giỏi Tin Học nữa ạ.  Lí do thì trước đấy e nghĩ cô cũng còn nhớ . </br> Em mong cô sẽ hiểu cho em . </br> Duy vẫn chưa biết gì đâu ạ, nên vẫn thường xuyên gửi để em ôn chứ không phai là em không ôn mà bỏ .</br> Vì lí do cá nhan trước e cx đã nói qua với cô rồi ạ. MONG CÔ THA THỨ, CHO ĐỨA HỌC SINH TỘI ĐỒ NÀY :((( 許していただけると幸いです.</br>  ( E hi vọng cô sẽ không nói vs Duy)",
+};
